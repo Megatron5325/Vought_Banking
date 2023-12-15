c3bd6ab3cb20b06961c11091b6a1f602e9a7af11
Megan Schleicher <chakras1012@gmail.com>
2023-12-15 13:51:44 -0500

    added functionality for instansiating, and resetting screen

--- bank.py
+++ bank.py
@@ -8,29 +8,41 @@ object
 '''
 
 import os
-import pyfiglet
+import time
 from colorama import Fore, Style
-from customer import Customer
-
-BANK_BANNER = pyfiglet.figlet_format("Vought Banking")
 
 class Bank:
     """a model of a bank"""
 
-    def __init__(self, main_menu_dict, logged_in_dict, company_banner):
-        self.main_menu_dict = main_menu_dict
-        self.logged_in_dict = logged_in_dict
+    def __init__(self, menu, company_banner):
+        self.menu = menu
         self.company_banner = company_banner
 
-    @staticmethod
-    def print_and_clear_screen(menu_options):
+    def reset_screen_print_main_menu(self):
         """prints pyfiglet banner"""
 
         # pyfiglet usage: will display banner for program
         os.system("clear")
-        print(Fore.BLUE + BANK_BANNER)
+        print(Fore.BLUE + self.company_banner)
         print(Style.RESET_ALL)
 
         print("You can type any menu item\n")
-        for item in menu_options:
+        for item in self.menu.main_menu_options:
+            print(item)
+
+    def reset_screen_print_logged_menu(self):
+        """prints all keys in the logged in dictionary"""
+
+        # user will only reach this point if they logged in successfully
+        print("\nLOGIN SUCCESS")
+        time.sleep(3)
+
+        # clear screen of main menu
+        # pyfiglet usage: will display banner for program
+        os.system("clear")
+        print(Fore.BLUE + self.company_banner)
+        print(Style.RESET_ALL)
+
+        print("You can type any menu item\n")
+        for item in self.menu.main_menu_options:
             print(item)
